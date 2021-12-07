6d93c16a53b66a66699658d8e606c914010ac156
Sam Iavri <sammi80134@gmail.com>
2021-12-06 17:40:46 -0700

    Ship exists and has a name

---
+++ spec/ship_spec.rb
@@ -0,0 +1,16 @@
+require './lib/ship'
+
+RSpec.describe Ship do
+  before(:each) do
+    @cruiser = Ship.new('Cruiser', 3)
+  end
+
+  it 'exists' do
+    expect(@cruiser).to be_an_instance_of(Ship)
+  end
+
+  it 'has a name' do
+    crusier = Ship.new('Cruiser', 3)
+    expect(@cruiser.name).to eq('Cruiser')
+  end
+end
