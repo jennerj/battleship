71f00107235b93af45eb9cbbb37de00f247052f8
Jacob Jenner <jennerjacob@gmail.com>
2021-12-06 18:39:18 -0700

    Write .coordinate test

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,2 +1,9 @@
 class Cell
+  attr_reader :coordinate, :ship
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = ship
+  end
+
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe Cell do
     @cell = Cell.new("B4")
   end
 
-  
+  it 'is a cell' do
+    expect(@cell).to be_an_instance_of(Cell)
+  end
+
+  it "can have a ship" do
+    expect(@cell.coordinate).to eq("B4")
+  end
 
 end
