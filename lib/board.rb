72747dd456c8ae0945f56efb5be7f99ef0befb28
Sam Iavri <sammi80134@gmail.com>
2021-12-09 16:01:00 -0700

    Valid placement method worksgit add .
    !

--- lib/board.rb
+++ lib/board.rb
@@ -3,23 +3,23 @@ class Board
 
   def initialize
     @cells = {
- "A1" => Cell.new("A1"),
- "A2" => Cell.new("A2"),
- "A3" => Cell.new("A3"),
- "A4" => Cell.new("A4"),
- "B1" => Cell.new("B1"),
- "B2" => Cell.new("B2"),
- "B3" => Cell.new("B3"),
- "B4" => Cell.new("B4"),
- "C1" => Cell.new("C1"),
- "C2" => Cell.new("C2"),
- "C3" => Cell.new("C3"),
- "C4" => Cell.new("C4"),
- "D1" => Cell.new("D1"),
- "D2" => Cell.new("D2"),
- "D3" => Cell.new("D3"),
- "D4" => Cell.new("D4"),
-}
+      'A1' => Cell.new('A1'),
+      'A2' => Cell.new('A2'),
+      'A3' => Cell.new('A3'),
+      'A4' => Cell.new('A4'),
+      'B1' => Cell.new('B1'),
+      'B2' => Cell.new('B2'),
+      'B3' => Cell.new('B3'),
+      'B4' => Cell.new('B4'),
+      'C1' => Cell.new('C1'),
+      'C2' => Cell.new('C2'),
+      'C3' => Cell.new('C3'),
+      'C4' => Cell.new('C4'),
+      'D1' => Cell.new('D1'),
+      'D2' => Cell.new('D2'),
+      'D3' => Cell.new('D3'),
+      'D4' => Cell.new('D4')
+    }
   end
 
   def valid_coordinate?(coordinate)
@@ -27,9 +27,8 @@ class Board
   end
 
   def valid_placement?(ship, coordinate)
-    coordinate.size == ship.length
-    linear?(coordinate.sort)
-
+    (coordinate.size == ship.length) &&
+      linear?(coordinate.sort)
   end
 
   def linear?(coordinate)
@@ -60,5 +59,19 @@ class Board
     (cell_1[0].ord - cell_2[0].ord).abs == 1
   end
 
+  def same_row?(coordinate)
+    if coordinate.length == 2
+      coordinate[0][0] == coordinate[1][0]
+    else
+      (coordinate[0][0] == coordinate[1][0]) && (coordinate[1][0] == coordinate[2][0])
+    end
+  end
 
+  def same_column?(coordinate)
+    if coordinate.length == 2
+      coordinate[0][1] == coordinate[1][1]
+    else
+      (coordinate[0][1] == coordinate[1][1]) && (coordinate[1][1] == coordinate[2][1])
+    end
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,12 +2,11 @@ require './lib/board'
 require './lib/cell'
 require './lib/ship'
 
-
 RSpec.describe Board do
   before(:each) do
     @board = Board.new
-    @cruiser = Ship.new("Cruiser", 3)
-    @submarine = Ship.new("Submarine", 2)
+    @cruiser = Ship.new('Cruiser', 3)
+    @submarine = Ship.new('Submarine', 2)
   end
 
   it 'exists' do
@@ -20,35 +19,44 @@ RSpec.describe Board do
   end
 
   it 'validates coordinate' do
-    expect(@board.valid_coordinate?("A1")).to eq(true)
-    expect(@board.valid_coordinate?("D4")).to eq(true)
-    expect(@board.valid_coordinate?("A5")).to eq(false)
-    expect(@board.valid_coordinate?("E1")).to eq(false)
-    expect(@board.valid_coordinate?("A22")).to eq(false)
+    expect(@board.valid_coordinate?('A1')).to eq(true)
+    expect(@board.valid_coordinate?('D4')).to eq(true)
+    expect(@board.valid_coordinate?('A5')).to eq(false)
+    expect(@board.valid_coordinate?('E1')).to eq(false)
+    expect(@board.valid_coordinate?('A22')).to eq(false)
   end
 
-  xit 'checks if the length is valid' do
-    expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to be(false)
-    expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to be(false)
+  it 'checks if the length is valid' do
+    expect(@board.valid_placement?(@cruiser, %w[A1 A2])).to be(false)
+    expect(@board.valid_placement?(@submarine, %w[A2 A3 A4])).to be(false)
   end
 
-  xit 'checks if the coordinates are consecutive' do
-    expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A3"])).to eq(true)
-    expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to eq(false)
-    expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to eq(false)
-    expect(@board.valid_placement?(@submarine, ["C1", "C2"])).to eq(true)
+  it 'checks if the coordinates are consecutive' do
+    expect(@board.valid_placement?(@cruiser, %w[A1 A2 A3])).to eq(true)
+    expect(@board.valid_placement?(@submarine, %w[A1 C1])).to eq(false)
+    expect(@board.valid_placement?(@cruiser, %w[A3 A2 A1])).to eq(true)
+    expect(@board.valid_placement?(@submarine, %w[C1 C2])).to eq(true)
+  end
+
+  it 'checks if the coordinates are not diagonal' do
+    expect(@board.valid_placement?(@cruiser, %w[A1 B2 C3])).to eq(false)
+    expect(@board.valid_placement?(@submarine, %w[C2 D3])).to eq(false)
+  end
+
+  it 'checks if the placement of the ship is valid' do
+    expect(@board.valid_placement?(@cruiser, %w[A1 A2 A3])).to eq(true)
+    expect(@board.valid_placement?(@submarine, %w[A1 C1])).to eq(false)
+    expect(@board.valid_placement?(@cruiser, %w[D4 C4 B4])).to eq(true)
+    expect(@board.valid_placement?(@submarine, %w[D1 B1])).to eq(false)
   end
 
   it 'checks is two cells are adjacent in a row' do
-    expect(@board.row_adjacent?("A3", "A4")).to be(true)
-    expect(@board.row_adjacent?("B1", "B2")).to be(true)
+    expect(@board.row_adjacent?('A3', 'A4')).to be(true)
+    expect(@board.row_adjacent?('B1', 'B2')).to be(true)
   end
 
   it 'checks is two cells are adjacent in a column' do
-    expect(@board.column_adjacent?("A3", "B3")).to be(true)
-    expect(@board.column_adjacent?("C1", "D1")).to be(true)
+    expect(@board.column_adjacent?('A3', 'B3')).to be(true)
+    expect(@board.column_adjacent?('C1', 'D1')).to be(true)
   end
-
-
-
 end
